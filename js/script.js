c3152a1d0847bd6a90f08a60d6e69c1231b93565
durw4rd <misa.fasa@gmail.com>
2016-05-19 22:39:29 +0200

    add logo with animations to lading page

--- js/script.js
+++ js/script.js
@@ -2,10 +2,12 @@
   var name = document.querySelector('.name');
   var claim = document.querySelector('.claim');
   var landingLine = document.querySelector('.landing-line');
+  var logo = document.querySelector('.logo')
 
   setTimeout(function(){
 		name.classList.add("landed");
     claim.classList.add("landed");
     landingLine.classList.add("landed");
+    logo.classList.add("landed");
   }, 500)
 })();
