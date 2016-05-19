94db22af1428fce7945ac86d86d1346a27f31cbd
durw4rd <misa.fasa@gmail.com>
2016-05-19 23:24:23 +0200

    create popup header thats initiated by scrolldown

--- js/script.js
+++ js/script.js
@@ -2,7 +2,10 @@
   var name = document.querySelector('.name');
   var claim = document.querySelector('.claim');
   var landingLine = document.querySelector('.landing-line');
-  var logo = document.querySelector('.logo')
+  var logo = document.querySelector('.logo');
+  var trigger = document.querySelector('#trigger');
+  var menu = document.querySelector('#popup-menu');
+
 
   setTimeout(function(){
 		name.classList.add("landed");
@@ -10,4 +13,16 @@
     landingLine.classList.add("landed");
     logo.classList.add("landed");
   }, 500)
+
+  new Waypoint({
+    element: document.querySelector('#trigger'),
+    handler: function(direction) {
+	  	if ( direction === "down" ) {
+	    	menu.classList.add("is-active");
+	    } else {
+	    	menu.classList.remove("is-active");
+	    }
+	  }
+  });
+
 })();
