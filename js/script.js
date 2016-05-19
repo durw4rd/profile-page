dc7d489f92bdbe49c2f78b4ac2416c47a2fef1ff
durw4rd <misa.fasa@gmail.com>
2016-05-19 16:12:29 +0200

    first commit: basic folder structure with supporting files; landing page with background and initial components including animations

---
+++ js/script.js
@@ -0,0 +1,11 @@
+(function(){
+  var name = document.querySelector('.name');
+  var claim = document.querySelector('.claim');
+  var landingLine = document.querySelector('.landing-line');
+
+  setTimeout(function(){
+		name.classList.add("landed");
+    claim.classList.add("landed");
+    landingLine.classList.add("landed");
+  }, 300)
+})();
